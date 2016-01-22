fd01a8fd887ef5a7cb38457a93e84369cfd6a3a3
Jeisiel10 <jeisiel10@hotmail.com>
2016-01-22 14:35:12 -0200

    Comparacao

---
+++ Comparacao/Conta.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comparacao
+{
+	class Conta
+	{
+		public int Numero { get; set; }
+		public double Saldo { get; set; }
+
+		public override bool Equals(object obj)
+		{
+			if (obj == this)
+			{
+				return true;
+			}
+			if (obj == null || obj.GetType() != this.GetType())
+			{
+				return false;
+			}
+			Conta x = obj as Conta;
+
+			return this.Numero == x.Numero;
+		}
+		public override int GetHashCode()
+		{
+			return this.GetHashCode();
+		}
+	}
+}
---
+++ Comparacao/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comparacao
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Conta a = new Conta();
+			a.Numero = 123;
+			a.Saldo = 5000;
+
+			Conta b = new Conta();
+			b.Numero = 123;
+			b.Saldo = 5000;
+
+			Console.WriteLine(a.Equals("olá"));
+			Console.WriteLine(a.Equals(a));
+			Console.WriteLine(a.Equals(null));
+			Console.WriteLine(a.Equals(b));
+
+
+		}
+	}
+}
